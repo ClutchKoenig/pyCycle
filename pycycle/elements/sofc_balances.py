1fd7ed6887047c88b359b8170a0c7fe99533cfef
ClutchKoenig <bastian.tellkamp@gmail.com>
2026-05-28 13:05:26 +0200

    classes now provide an option to choose active or passive segment model

--- pycycle/elements/sofc_balances.py
+++ pycycle/elements/sofc_balances.py
@@ -14,49 +14,73 @@ class PENEnergyBalance(om.ImplicitComponent):
     Residual: Qdot_conv_an + Qdot_conv_cat - Qdot_chem - P_elec + Qdot_cond_left + Qdot_cond_right = 0
     T_cell is the implicit state solved by the Newton solver.
     """
+    def initialize(self):
+        self.options.declare('seg_type', values=['passive', 'active'])
 
     def setup(self):
+
         self.add_input('Qdot_conv_PEN_an',    val=0.0, units='W', desc='Convective heat from anode to PEN')
         self.add_input('Qdot_conv_PEN_cat',   val=0.0, units='W', desc='Convective heat from cathode to PEN')
-        self.add_input('Qdot_chem',           val=0.0, units='W', desc='Chemical heat release')
+        
         self.add_input('Qdot_conduct_PEN_left',  val=0.0, units='W', desc='Conductive heat flux, left')
         self.add_input('Qdot_conduct_PEN_right', val=0.0, units='W', desc='Conductive heat flux, right')
-        self.add_input('I',      val=1.0,  units='A',   desc='Current')
-        self.add_input('V_cell', val=0.7,  units='V',   desc='Cell voltage')
+        
         self.add_input('N_cell', val=1.0,  units=None,  desc='Number of cells')
 
         self.add_output('T_cell',        val=1001.9, units='K', desc='PEN temperature')
         #self.add_output('PEN_residuum',  val=0.0,   units='W', desc='Energy balance residual')
+        passive_inputs = ['Qdot_conv_PEN_an', 'Qdot_conv_PEN_cat', 
+                        'Qdot_conduct_PEN_left', 'Qdot_conduct_PEN_right']
+        
+        seg_type = self.options['seg_type']
 
-        self.declare_partials('T_cell', ['Qdot_conv_PEN_an', 'Qdot_conv_PEN_cat', 'Qdot_chem',
-                                         'Qdot_conduct_PEN_left', 'Qdot_conduct_PEN_right',
-                                         'I', 'V_cell', 'N_cell'])
+        if seg_type == 'active':
+            self.add_input('Qdot_chem',val=0.0, units='W', desc='Chemical heat release')
+            self.add_input('I',        val=1.0, units='A', desc='Current')
+            self.add_input('V_cell',   val=0.7, units='V', desc='Cell voltage')
+            active_inputs = ['Qdot_chem', 'I', 'V_cell', 'N_cell']
+            self.declare_partials('T_cell', active_inputs)
+
+        self.declare_partials('T_cell', passive_inputs)
         self.declare_partials('T_cell', 'T_cell', val=0.0)
         # self.declare_partials('PEN_residuum', ['Qdot_conv_PEN_an', 'Qdot_conv_PEN_cat', 'Qdot_chem',
         #                                        'Qdot_conduct_PEN_left', 'Qdot_conduct_PEN_right',
         #                                        'I', 'V_cell', 'N_cell'])
 
     def apply_nonlinear(self, inputs, outputs, residuals):
-        P_elec = inputs['I'] * inputs['V_cell'] * inputs['N_cell']
-        energy_balance = (inputs['Qdot_conv_PEN_an'] +
-                          inputs['Qdot_conv_PEN_cat'] -
-                          inputs['Qdot_chem'] -
-                          P_elec +
-                          inputs['Qdot_conduct_PEN_left'] +
-                          inputs['Qdot_conduct_PEN_right'])
+        seg_type = self.options['seg_type']
 
+        Q_conv_an = inputs['Qdot_conv_PEN_an']
+        Q_conv_cat = inputs['Qdot_conv_PEN_cat']
+        Q_cond_left = inputs['Qdot_conduct_PEN_left']
+        Q_cond_right = inputs['Qdot_conduct_PEN_right']
+
+        if seg_type == 'active':
+            P_elec = inputs['I'] * inputs['V_cell'] * inputs['N_cell']
+            Q_chem = inputs['Qdot_chem']
+
+        elif seg_type == 'passive':
+            P_elec = 0
+            Q_chem = 0
+
+        energy_balance = ( Q_conv_cat + Q_conv_an - Q_chem - P_elec + Q_cond_left +Q_cond_right)
         residuals['T_cell'] = energy_balance
+
         #outputs['PEN_residuum'] = energy_balance
 
     def linearize(self, inputs, outputs, J):
-        J['T_cell', 'Qdot_conv_PEN_an']      = 1.0
-        J['T_cell', 'Qdot_conv_PEN_cat']     = 1.0
-        J['T_cell', 'Qdot_chem']             = -1.0
-        J['T_cell', 'Qdot_conduct_PEN_left'] = 1.0
-        J['T_cell', 'Qdot_conduct_PEN_right']= 1.0
-        J['T_cell', 'I']                     = -inputs['V_cell'] * inputs['N_cell']
-        J['T_cell', 'V_cell']                = -inputs['I'] * inputs['N_cell']
-        J['T_cell', 'N_cell']                = -inputs['I'] * inputs['V_cell']
+        seg_type = self.options['seg_type']
+
+        J['T_cell', 'Qdot_conv_PEN_an']       = 1.0
+        J['T_cell', 'Qdot_conv_PEN_cat']      = 1.0
+        J['T_cell', 'Qdot_conduct_PEN_left']  = 1.0
+        J['T_cell', 'Qdot_conduct_PEN_right'] = 1.0
+
+        if seg_type == 'active':
+            J['T_cell', 'N_cell']    = -inputs['I'] * inputs['V_cell']
+            J['T_cell', 'V_cell']    = -inputs['I'] * inputs['N_cell']
+            J['T_cell', 'I']         = -inputs['V_cell'] * inputs['N_cell']
+            J['T_cell', 'Qdot_chem'] = -1.0
 
         # # PEN_residuum shares the same derivatives
         # for key in ['Qdot_conv_PEN_an', 'Qdot_conv_PEN_cat', 'Qdot_chem',
@@ -68,9 +92,17 @@ class PENEnergyBalance(om.ImplicitComponent):
 class ICEnergyBalance(om.ImplicitComponent):
     """
     Steady-state energy balance for the interconnect
-    R = Qdot_conv_IC_an + Qdot_conv_IC_cat -  Q_dot_loss + Qdot_conduct_IC_left + Qdot_conduct_IC_right 
+    R = Qdot_conv_IC_an + Qdot_conv_IC_cat -  Q_dot_loss + Qdot_conduct_IC_left + Qdot_conduct_IC_right
+    
+    For the IC the segment type doesnt matter. No chemical reaction or Current Flows.
+    I think a source term for joule heating since current is flowing in the IC. 
+    Q_joule = I**2 * A_cell * ASR_IC ? 
     """
+    def initalize(self):
+        self.options.declare('seg_type', default='passive', values=['active', 'passive'])
     def setup(self):
+        seg_type = self.options['seg_type']
+
         self.add_input('Qdot_conv_IC_an', val=0, units='W', desc= 'Convective heat from anode to IC')
         self.add_input('Qdot_conv_IC_cat', val=0, units='W', desc= 'Convective heat from cathode to IC')
         self.add_input('Q_dot_loss', val=0, units='W') # need a seperate component to calculate q_loss using Power and percent heat loss
