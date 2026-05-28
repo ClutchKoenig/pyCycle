14acf2057325639ee12101cc816adeb69a677df6
ClutchKoenig <bastian.tellkamp@gmail.com>
2026-05-28 20:34:03 +0200

    Easy access to Key Components for SOFC Modelling

---
+++ pycycle/sofc_api.py
@@ -0,0 +1,5 @@
+
+from pycycle.elements.sofc_balances import ChannelEnergyBalance, PENEnergyBalance, ICEnergyBalance, ChannelMassBalance
+from pycycle.elements.sofc_heat import HeatConduction, HeatConvection
+from pycycle.elements.sofc_reaction import ElectroChemistry
+from pycycle.elements.sofc_thermo_add import SOFCThermoAdd
