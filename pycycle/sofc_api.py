492cbdc48f34bbebf57c8538cf41203496297d9a
ClutchKoenig <bastian.tellkamp@gmail.com>
2026-05-29 14:40:54 +0200

    added sofc_observables.py

--- pycycle/sofc_api.py
+++ pycycle/sofc_api.py
@@ -1,5 +1,6 @@
 
-from pycycle.elements.sofc_balances import ChannelEnergyBalance, PENEnergyBalance, ICEnergyBalance, ChannelMassBalance
+from pycycle.elements.sofc_balances import ChannelEnergyBalance, PENEnergyBalance, ICEnergyBalance, SpeciesFlowCalc
 from pycycle.elements.sofc_heat import HeatConduction, HeatConvection
 from pycycle.elements.sofc_reaction import ElectroChemistry
 from pycycle.elements.sofc_thermo_add import SOFCThermoAdd
+from pycycle.elements.sofc_observables import SpeciesUtilization, MassSanityCheck
