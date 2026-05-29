6fb7b4336ff3da1a9aad1a506d0a24f20a6f1cf0
ClutchKoenig <bastian.tellkamp@gmail.com>
2026-05-29 10:52:29 +0200

    changed sofc_passive.py to sofc_observables.py - Holds components that have no influence on the actual physics

--- pycycle/elements/sofc_observables.py
+++ pycycle/elements/sofc_observables.py
@@ -8,3 +8,12 @@ from pycycle.flow_in import FlowIn
 
 RM = 8.3145                    # J/ (mol K)
 FARADAY =  96485.3321233100184 # C/mol = As/mol
+
+class utilization(om.ExplicitComponent):
+    """
+    Calculates the Oxygen/Fuel utilization
+    """
+
+    def setup(self):
+        self.add_input('n', units=None, desc='The base_thermo.thermo.n mole vector n [mol/g] ')
+        
