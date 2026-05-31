04c591eba8c15c7bdf558397132a0b6c11bb2ea4
ClutchKoenig <bastian.tellkamp@gmail.com>
2026-05-31 20:03:08 +0200

    adding SegmentSOFC()

--- pycycle/sofc_api.py
+++ pycycle/sofc_api.py
@@ -4,3 +4,4 @@ from pycycle.elements.sofc_heat import HeatConduction, HeatConvection
 from pycycle.elements.sofc_reaction import ElectroChemistry
 from pycycle.elements.sofc_thermo_add import SOFCThermoAdd
 from pycycle.elements.sofc_observables import SpeciesUtilization, MassSanityCheck, BulkComposition
+from pycycle.elements.sofc_segment import SegmentSOFC
